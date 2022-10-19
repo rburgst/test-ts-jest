c97182805da19efc87045b0e0c254dc9de21f57a
Rainer Burgstaller <rainer.burgstaller@e-mundo.de>
2022-10-19 18:41:54 +0200

    fix: update config as suggested
    - still no joy

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,8 @@
 import type { Config } from "jest";
 
 const config: Config = {
-  preset: "ts-jest/presets/default-esm",
   testEnvironment: "jsdom",
   // [...]
-  extensionsToTreatAsEsm: [".ts"],
   transform: {
     "^.+\\.tsx?$": [
       "ts-jest",
